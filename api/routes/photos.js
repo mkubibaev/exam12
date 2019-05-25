ee1e81edf1cb662a83f7b6475ce97804822e6ed9
mkubibaev <mkubibaev@gmail.com>
2019-05-25 16:37:50 +0600

    #1 Удаление фотографий на сервере

--- api/routes/photos.js
+++ api/routes/photos.js
@@ -42,4 +42,20 @@ router.post('/', [auth, upload.single('image')], async (req, res) => {
     }
 });
 
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const photo = await Photo.findById(req.params.id);
+        
+        if (photo.user.equals(req.user._id)) {
+            await photo.remove()
+
+            return res.status(200).send({message: 'Photo deleted!'});
+        } else {
+            return res.status(403).send({message: 'Access forbidden!'});
+        }
+    } catch (error) {
+        return res.status(400).send(error);
+    }
+});
+
 module.exports = router;
